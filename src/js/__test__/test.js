a97ab845fc0e73ad04329bc377f29d442ed9a595
Garysi <garysi@rambler.ru>
2023-06-15 13:55:13 +0300

    add classes and tests

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -1,23 +1,103 @@
-import Validator from '../basic.js';
+import Character from '../character';
+import Team from '../team';
+import ErrorRepository from '../error';
 
 
-test('test username ', () => {
-  let result = Validator.validateUsername(12345);
-  expect(result).toEqual(false);
+/////////////////////////////////////////////////////////////////// team
 
-  result = Validator.validateUsername('gary');
-  expect(result).toEqual(true);
+test('add uncorrect type', () => {
+  const char = 'john';
+  const team = new Team();
+  let messageError = 'empty';
 
-  result = Validator.validateUsername('_gary');
-  expect(result).toEqual(false);
+  try {
+    team.add(char);
+  } catch (error) {
+    messageError = String(error);
+  }
 
-  result = Validator.validateUsername('gary12345');
-  expect(result).toEqual(false);
+  expect(messageError).toBe('TypeError');
+});
 
-  result = Validator.validateUsername('tik-tok777q');
-  expect(result).toEqual(true);
 
-  result = Validator.validateUsername('tik_tok777w');
-  expect(result).toEqual(true);
+test('add exist character', () => {
+  const char = new Character('john');
+  const team = new Team();
+  let messageError = 'empty';
 
+  try {
+    team.add(char);
+    team.add(char);
+  } catch (error) {
+    messageError = String(error);
+  }
+
+  expect(messageError).toBe('Error');
+});
+
+
+test('add character', () => {
+  const char = new Character('john');
+  const team = new Team();
+
+  team.add(char);
+
+  expect(team.members).toEqual(new Set([char]));
+});
+
+
+test('add correct characters list', () => {
+  const firstChar = new Character('john');
+  const secondChar = new Character('smith');
+  const team = new Team();
+
+  team.addAll(firstChar, secondChar, firstChar);
+
+  expect(team.members).toEqual(new Set([firstChar, secondChar]));
+});
+
+
+test('add uncorrect characters list', () => {
+  const firstChar = new Character('john');
+  const secondChar = 'smith';
+  const team = new Team();
+  let messageError = 'empty';
+
+  try {
+    team.addAll(firstChar, secondChar, firstChar);
+  } catch (error) {
+    messageError = String(error);
+  }
+
+  expect(messageError).toBe('TypeError');
+});
+
+
+test('toArray', () => {
+  const firstChar = new Character('john');
+  const secondChar = new Character('smith');
+  const team = new Team();
+
+  team.members = new Set([firstChar, secondChar]);
+
+  expect(team.toArray()).toEqual([firstChar, secondChar]);
+});
+
+
+/////////////////////////////////////////////////////////////////// error
+
+test('get correct code description', () => {
+  const code = 200;
+  const errorRepository = new ErrorRepository();
+
+  const actual = errorRepository.translate(code);
+  expect(actual).toBe('ok');
+});
+
+test('get uncorrect code description', () => {
+  const code = 287;
+  const errorRepository = new ErrorRepository();
+
+  const actual = errorRepository.translate(code);
+  expect(actual).toBe('Unknown error');
 });
